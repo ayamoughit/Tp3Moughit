ae6fb15fefd97ea9757a5871ee5a7864ee3ca06f
Moughit aya <Aya.Moughit@emsi-edu.ma>
2025-11-04 15:34:02 +0100

    Ajout de @PathParam, retour Response et test d’erreur serveur.

--- src/main/java/ma/emsi/moughit/tp3moughit/HelloResource.java
+++ src/main/java/ma/emsi/moughit/tp3moughit/HelloResource.java
@@ -2,13 +2,19 @@ package ma.emsi.moughit.tp3moughit;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
+    @Path("personnes/{nom}")
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    public Response hello(@PathParam("nom") String nom) {
+         return Response.ok("Hello, " + nom).build();
+        /*return Response.serverError()
+                .entity("Internal Server Error")
+                .build();*/
     }
 }
